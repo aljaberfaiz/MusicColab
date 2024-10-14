08750e79d949ba735ad86ce834ff3a0397b3e363
soostroy <soostroy2004@gmail.com>
2024-10-14 15:47:34 +0200

    Initial commit

---
+++ src/main/java/com/example/musiccolab/MusicColabApplication.java
@@ -0,0 +1,13 @@
+package com.example.musiccolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicColabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicColabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musiccolab/MusicColabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musiccolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicColabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
