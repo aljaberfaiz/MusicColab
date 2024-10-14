be192d77db53b15622b251d49039333f4303660e
soostroy <soostroy2004@gmail.com>
2024-10-14 21:14:27 +0200

    database complete but no preview

--- src/main/java/com/example/musiccolab/MusicColabApplication.java
+++ src/main/java/com/example/musiccolab/MusicColabApplication.java
@@ -1,13 +1,21 @@
 package com.example.musiccolab;
 
+import Model.Composer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MusicColabApplication {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(MusicColabApplication.class, args);
+        Composer composer = new Composer();
+        composer.setFirstName("John");
+        composer.setLastName("Smith");
+        composer.setEmail("john.smith@gmail.com");
+        composer.setGenre("Donkey");
+        composer.setPhone("+931903824920384");
+
     }
 
 }
